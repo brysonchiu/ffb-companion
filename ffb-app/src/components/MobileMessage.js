1216915cf4ab2a4cd2f60e94a2b582e589542a84
Bryson Chiu <bryson.chiu@gmail.com>
2021-07-21 15:25:27 -0500

    update stats; fix mobile check typo, margins on ios, and svg console errors

--- ffb-app/src/components/MobileMessage.js
+++ ffb-app/src/components/MobileMessage.js
@@ -5,7 +5,7 @@ export function MobileMessage() {
     <div className="mobile-message__container">
       <BrowserWindow />
       <h2 className="mobile-message__title">{`Right now, bigger${"\u00a0"}is${"\u00a0"}better.`}</h2>
-      <p className="mobile-message">This site is optimized for larger broser windows. Switch to desktop or adjust your window.</p>
+      <p className="mobile-message">This website is optimized for larger browser windows. Switch to desktop or adjust your window.</p>
     </div>
   );
 }
--- ffb-app/src/components/icons/browser-window.js
+++ ffb-app/src/components/icons/browser-window.js
@@ -1,16 +1,16 @@
 export function BrowserWindow() {
   return (
     <svg width="96" height="73" fill="none" xmlns="http://www.w3.org/2000/svg" className="icon-browser-window">
-      <path clip-rule="evenodd" d="M2 71h92V2H2v69z" stroke="#FFFFFE" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-      <path d="M94 24H2" stroke="#FFFFFE" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M84 13a3 3 0 11-6 0 3 3 0 016 0z" fill="#00EBC7" />
+      <path clipRule="evenodd" d="M2 71h92V2H2v69z" stroke="#FFFFFE" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M94 24H2" stroke="#FFFFFE" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M84 13a3 3 0 11-6 0 3 3 0 016 0z" fill="#00EBC7" />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M73 13c0 1.657-1.567 3-3.5 3-1.932 0-3.5-1.343-3.5-3 0-1.656 1.568-3 3.5-3 1.933 0 3.5 1.344 3.5 3z"
         fill="#FDE24F"
       />
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M61 13a3 3 0 11-6 0 3 3 0 016 0z" fill="#FF5470" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M61 13a3 3 0 11-6 0 3 3 0 016 0z" fill="#FF5470" />
       <path
         className="icon-left-right-arrows"
         d="M53.978 43.092v2.092H43.022v-2.092c0-.97-1.161-1.457-1.84-.77l-3.866 3.907a1.098 1.098 0 000 1.542l3.866 3.908c.679.686 1.84.2 1.84-.771v-2.092h10.956v2.092c0 .97 1.161 1.457 1.84.77l3.866-3.907a1.098 1.098 0 000-1.542l-3.866-3.908c-.679-.686-1.84-.2-1.84.771z"
